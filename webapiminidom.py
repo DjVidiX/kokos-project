59053e39025423c6d061216d3cb1b84bec54f87e
Adam Wilkowski <aw34917@st.amu.edu.pl>
2013-09-24 15:03:47 +0200

    webapiminidom.py update

--- webapiminidom.py
+++ webapiminidom.py
@@ -94,7 +94,7 @@ class WebAPI:
                     elif (str(element.nodeName) == 'financialVerifies'):
                         tempAuction['verify'] = len(element.childNodes)
             tempAuction['risk'] = round((self.calcRisk(tempAuction) - 1) * 100)
-            tempAuction['id'] = auction.getElementsByTagName('id')[0].childNodes[0].toprettyxml().encode('UTF-8'))
+            tempAuction['id'] = auction.getElementsByTagName('id')[0].childNodes[0].toprettyxml().encode('UTF-8')
             tempAuction['url'] = QtCore.QString('https://kokos.pl/aukcje?id=' + tempAuction['id'])
             auctions.append(tempAuction)
             tempAuction = {}
