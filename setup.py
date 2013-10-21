cb1d8fd3fd8912656e02a22c273091f32525a7f1
adasikow <adasikow@gmail.com>
2013-10-22 01:55:17 +0200

    setup.py update
    Działa obrazek w tle. Generowanie exe:
    'python setup.py py2exe'
    wygenerowane pliki zostają utworzone w katalogu 'dist'
    Plik qtjpeg4.dll jest potrzebny do obsługi plików jpeg (w tym przypadku
    - obrazka w tle)
    Ikony kokosa niestety nie udało mi się zaimplementować

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-Mydata_files = [('.', ['plaza.png', 'kokos.png'])]
+Mydata_files = [('images', ['plaza.jpg', 'kokos.png']), ('images', ['.\\imageformats\\qjpeg4.dll'])]
 
 setup(
 	windows=[{"script":"wywolanieGui.py"}],
