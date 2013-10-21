6e14876ceca7cb5404842dcc04026d1eec048674
adasikow <adasikow@gmail.com>
2013-10-22 01:32:55 +0200

    setup.py update (py2exe)

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 import py2exe
+
+Mydata_files = [('.', ['plaza.png', 'kokos.png'])]
+
 setup(
-    version = "1.0",
-    #windows = ["skrypt.py"],
-    console = ["skrypt.py"],
-    )
+	windows=[{"script":"wywolanieGui.py"}],
+	data_files = Mydata_files,
+	options={"py2exe":{"includes":["sip"]}})
